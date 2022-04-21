b675544b930eea78de1d53b4044849870af1e315
Omisha Patel <omishapatel@gmail.com>
2022-04-21 10:27:13 -0400

    adding tests

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
 from . import models
-
+from . database import engine
 from fastapi import FastAPI
 from .routers import post, user, auth, vote
 from .config import settings
@@ -9,15 +9,7 @@ app = FastAPI()
 
 
 # models.Base.metadata.create_all(bind=engine)
-origins = ["*"]
 
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
 app.include_router(post.router)
 app.include_router(user.router)
 app.include_router(auth.router)
